88a17c62bf4c93c0ae32dc0b9f88b4bf99f12cb0
sunny00217wm <sunnu00217wm@gmail.com>
2021-02-20 20:37:30 +0800

    =

--- src/list.js
+++ src/list.js
@@ -2,13 +2,7 @@ module.exports = [
 	{
 		"module": "SickManEmpireTime",
 		"version": "1.0.0",
-		"src": "SickManEmpire/SickManEmpireTime.js",
+		"src": "SickManEmpireTime.js",
 		"output": "SickManEmpireTime"
-	},
-	{
-		"module": "LokmandieEmpireTime",
-		"version": "1.0.0",
-		"src": "LokmandieEmpire/LokmandieEmpireTime.js",
-		"output": "LokmandieEmpireTime"
 	}
 ];
